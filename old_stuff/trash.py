827358fd75dc6404ad06dcd724e3426290fe532b
isole-01 <tooraanian@gmail.com>
2024-11-20 15:19:08 +0100

    automatic metadata extraction

--- old_stuff/trash.py
+++ old_stuff/trash.py
@@ -1,3 +1,35 @@
+
+
+from llama_index.core.node_parser import SentenceSplitter
+
+text_parser = SentenceSplitter(
+    chunk_size=512,
+    # separator=" ",
+)
+
+text_chunks = []
+# maintain relationship with source doc index, to help inject doc metadata in (3)
+doc_idxs = []
+for doc_idx, doc in enumerate(documents):
+    cur_text_chunks = text_parser.split_text(doc.text)
+    text_chunks.extend(cur_text_chunks)
+    doc_idxs.extend([doc_idx] * len(cur_text_chunks))
+
+##########################
+
+from llama_index.core.schema import TextNode
+
+nodes = []
+for idx, text_chunk in enumerate(text_chunks):
+    node = TextNode(
+        text=text_chunk,
+    )
+    src_doc = documents[doc_idxs[idx]]
+    node.metadata = src_doc.metadata
+    nodes.append(node)
+
+    
+
 def retrieve_data(query):
     # right now this function doesn't use the query, but it could be used to filter the data later...
     with open(DATA_PATH, 'r') as file:
